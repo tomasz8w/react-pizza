031c60a6bfda0e61eacd721429dae1e4a0c6059b
tomasz8w <td.wieclaw@gmail.com>
2021-02-09 10:22:16 +0100

    added key parameter to component

--- src/Content.js
+++ src/Content.js
@@ -3,7 +3,7 @@ import PizzaItem from './PizzaItem'
 function Content(props){
     return (
       <div className="Content">
-        {props.data.map(data => <PizzaItem pizza={data} />)}
+        {props.data.map(data => <PizzaItem pizza={data} key={data.id}/>)}
       </div>);
   };
 
