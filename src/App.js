01b59a7b11abc176c78d064043f57fa227442fdc
tomasz8w <td.wieclaw@gmail.com>
2021-02-09 10:24:08 +0100

    import instead require when loading json data

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import Header from './Header'
 import Footer from './Footer'
 import Content from './Content'
 import Menu from './Menu'
+import data from './data.json'
 
 function App() {
 
-  const dataFile = require("./data.json");
   return (
     <div className="App">
       <Header />
       <Menu />
-      <Content data={dataFile}/>
+      <Content data={data}/>
       <Footer />
     </div>
     
