5962fee36ba6f24e2743185fad35e78b405b53cd
tomasz8w <td.wieclaw@gmail.com>
2021-02-15 05:07:28 -0800

    Filter vegetarian pizzas

--- src/Content.js
+++ src/Content.js
@@ -1,36 +1,46 @@
 import PizzaItem from './PizzaItem';
 import { useState } from "react";
-import { FormControl, Grid, InputLabel, makeStyles, MenuItem, Select } from '@material-ui/core'
+import { Checkbox, FormControl, FormHelperText, Grid, InputLabel, makeStyles, MenuItem, Select } from '@material-ui/core'
 
 const useStyles = makeStyles((theme) => ({
   formControl: {
     margin: theme.spacing(2)
-  }
+  },
+
 }));
 
 function Content(props) {
   const classes = useStyles();
 
   const [selectedSize, setSelectedSize] = useState("1");
+  const [onlyVegan, setOnlyVegan] = useState(false);
 
   return (
     <Grid container direction="row" spacing={2}>
       <Grid item xs={12} >
-        <FormControl className={classes.formControl}>
+        <FormControl className={classes.formControl} >
           <InputLabel>Rozmiar dania</InputLabel>
           <Select
             onChange={(event) => {
               setSelectedSize(event.target.value);
             }}
-            value={selectedSize}
-          >
+            value={selectedSize} >
             <MenuItem value="0" >Maly</MenuItem>
             <MenuItem value="1">Sredni</MenuItem>
             <MenuItem value="2">Duzy</MenuItem>
           </Select>
         </FormControl>
+        <FormControl className={classes.formControl}>
+          <FormHelperText>Vege</FormHelperText>
+          <Checkbox checked={onlyVegan} onChange={(event) => setOnlyVegan(event.target.checked)} />
+
+        </FormControl>
       </Grid>
-      {props.data.map(pizza => <PizzaItem pizza={pizza} key={pizza.id} selectedSize={selectedSize} />)}
+
+      {
+        (onlyVegan ? props.data.filter(pizza => pizza.vegetarian) : props.data)
+          .map(pizza => <PizzaItem pizza={pizza} key={pizza.id} selectedSize={selectedSize} />)
+      }
     </Grid>
   )
 };
