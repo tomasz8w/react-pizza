0f2f49fcb6525f52726a581f99f7b46b19ba51a3
tomasz8w <td.wieclaw@gmail.com>
2021-02-16 21:11:41 +0100

    feat: change quantity on cart page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import Header from './Header'
-import Footer from './Footer'
-import Content from './Content'
-import Navigation from './Navigation'
-import Cart from './Cart'
-import data from './data.json'
-import { useState, useEffect } from 'react'
-import { Grid } from '@material-ui/core'
+import Header from "./Header";
+import Footer from "./Footer";
+import Content from "./Content";
+import Navigation from "./Navigation";
+import Cart from "./Cart";
+import data from "./data.json";
+import { useState, useEffect } from "react";
+import { Grid } from "@material-ui/core";
 import { Switch, Route } from "react-router-dom";
 
 function App() {
@@ -23,24 +23,40 @@ function App() {
     setCart((items) => {
       const productInCart = items.find((p) => p.id === id && p.size === size);
       if (productInCart) {
-        return items.map(i =>
-          (i.id === id && i.size === size) ? { ...i, quantity: i.quantity + 1 } : i
+        return items.map((i) =>
+          i.id === id && i.size === size
+            ? { ...i, quantity: i.quantity + 1 }
+            : i
         );
       } else {
         return [...items, { id, size, quantity: 1 }];
       }
-    })
+    });
   }
 
   function deleteFromCart(id, size) {
     setCart((items) => {
-      return items.filter(i => (i.id !== id) || (i.id === id && i.size !== size))
-    })
+      return items.filter(
+        (i) => i.id !== id || (i.id === id && i.size !== size)
+      );
+    });
+  }
+
+  function changeQuantity(id, size, changeQuantityStep) {
+    setCart((items) => {
+      const productInCart = items.find((p) => p.id === id && p.size === size);
+      if (productInCart.quantity + changeQuantityStep > 0) {
+        return items.map((i) =>
+          i.id === id && i.size === size
+            ? { ...i, quantity: i.quantity + changeQuantityStep }
+            : i
+        );
+      } else return items;
+    });
   }
 
   return (
-    <Grid container direction='column'>
-
+    <Grid container direction="column">
       <Grid item xs={12}>
         <Header />
       </Grid>
@@ -50,23 +66,25 @@ function App() {
           <Navigation />
         </Grid>
         <Grid item xs={9} sm={9}>
-
           <Switch>
             <Route exact path="/">
               <Content addToCart={addToCart} data={data} />
             </Route>
             <Route path="/cart">
-              <Cart cart={cart} data={data} deleteFromCart={deleteFromCart} />
+              <Cart
+                cart={cart}
+                data={data}
+                deleteFromCart={deleteFromCart}
+                changeQuantity={changeQuantity}
+              />
             </Route>
           </Switch>
-
         </Grid>
       </Grid>
 
       <Grid container item xs={12} justify="center">
         <Footer />
       </Grid>
-
     </Grid>
   );
 }
--- src/Cart.js
+++ src/Cart.js
@@ -1,88 +1,118 @@
 import { Paper, Typography, Grid, makeStyles } from "@material-ui/core";
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import IconButton from '@material-ui/core/IconButton';
-import DeleteIcon from '@material-ui/icons/Delete';
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import IconButton from "@material-ui/core/IconButton";
+import DeleteIcon from "@material-ui/icons/Delete";
+import AddIcon from "@material-ui/icons/Add";
+import RemoveIcon from "@material-ui/icons/Remove";
 
-const useStyles = makeStyles((theme) => (
-    {
-        paper: {
-            backgroundColor: theme.palette.primary.light,
-            color: theme.palette.primary.contrastText
-        }
-    }
-))
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    backgroundColor: theme.palette.primary.light,
+    color: theme.palette.primary.contrastText,
+  },
+}));
 
 export default function Cart(props) {
-    const cart = props.cart;
-    const data = props.data;
-    const deleteFromCart = props.deleteFromCart;
+  const cart = props.cart;
+  const data = props.data;
+  const changeQuantity = props.changeQuantity;
+  const deleteFromCart = props.deleteFromCart;
 
-    const classes = useStyles();
+  const classes = useStyles();
 
-    const cartSumValue = cart.map((cartElement) => (
-        data.filter(d => d.id === cartElement.id)
-            .map(d => d.price[cartElement.size])
-    )).reduce((sum, e) => sum + e[0], 0);
+  const cartSumValue = cart
+    .map((cartElement) =>
+      data
+        .filter((d) => d.id === cartElement.id)
+        .map((d) => d.price[cartElement.size])
+    )
+    .reduce((sum, e) => sum + e[0], 0);
 
-    return (
-        <Grid container style={{ "margin": "10px", "padding": "10px" }} direction="row" justify="center">
-            <Grid item xs={12} md={8} >
-                <Paper align="center" className={classes.paper}>
-                    <Typography variant="h4">KOSZYK</Typography>
+  return (
+    <Grid
+      container
+      style={{ margin: "10px", padding: "10px" }}
+      direction="row"
+      justify="center"
+    >
+      <Grid item xs={12} md={8}>
+        <Paper align="center" className={classes.paper}>
+          <Typography variant="h4">KOSZYK</Typography>
 
-                    <TableContainer component={Paper}>
-                        <Table>
-                            <TableHead>
-                                <TableRow>
-                                    <TableCell>Id</TableCell>
-                                    <TableCell>Nazwa</TableCell>
-                                    <TableCell>Rozmiar</TableCell>
-                                    <TableCell>Ilość</TableCell>
-                                    <TableCell>Cena</TableCell>
-                                    <TableCell />
-                                </TableRow>
-                            </TableHead>
+          <TableContainer component={Paper}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Id</TableCell>
+                  <TableCell>Nazwa</TableCell>
+                  <TableCell>Rozmiar</TableCell>
+                  <TableCell>Ilość</TableCell>
+                  <TableCell>Cena</TableCell>
+                  <TableCell />
+                </TableRow>
+              </TableHead>
 
+              <TableBody>
+                {cart.map((i) => (
+                  <TableRow key={`${i.id}_${i.size}`}>
+                    <TableCell>{i.id}</TableCell>
+                    <TableCell>
+                      {data
+                        .filter((pizza) => pizza.id === i.id)
+                        .map((pizza) => pizza.name)}
+                    </TableCell>
+                    <TableCell>{i.size}</TableCell>
+                    <TableCell>
+                      <IconButton
+                        onClick={() => {
+                          changeQuantity(i.id, i.size, -1);
+                        }}
+                      >
+                        <RemoveIcon fontSize="small" />
+                      </IconButton>
+                      {i.quantity}
+                      <IconButton
+                        onClick={() => {
+                          changeQuantity(i.id, i.size, 1);
+                        }}
+                      >
+                        <AddIcon fontSize="small" />
+                      </IconButton>
+                    </TableCell>
+                    <TableCell>
+                      {data
+                        .filter((pizza) => pizza.id === i.id)
+                        .map((pizza) => pizza.price[i.size]) *
+                        i.quantity +
+                        " zł"}
+                    </TableCell>
+                    <TableCell>
+                      <IconButton
+                        onClick={() => {
+                          deleteFromCart(i.id, i.size);
+                        }}
+                      >
+                        <DeleteIcon />
+                      </IconButton>
+                    </TableCell>
+                  </TableRow>
+                ))}
 
-                            <TableBody>
-                                {
-                                    cart.map((i) =>
-                                    (
-                                        <TableRow key={`${i.id}_${i.size}`}>
-                                            <TableCell>{i.id}</TableCell>
-                                            <TableCell>{data.filter((pizza) => pizza.id === i.id).map((pizza) => pizza.name)}</TableCell>
-                                            <TableCell>{i.size}</TableCell>
-                                            <TableCell>{i.quantity}</TableCell>
-                                            <TableCell>
-                                                {
-                                                    data.filter((pizza) => pizza.id === i.id)
-                                                        .map((pizza) => pizza.price[i.size]) * i.quantity + " zł"
-                                                }
-                                            </TableCell>
-                                            <TableCell>
-                                                <IconButton onClick={() => { deleteFromCart(i.id, i.size) }} >
-                                                    <DeleteIcon />
-                                                </IconButton>
-                                            </TableCell>
-                                        </TableRow>
-                                    ))
-                                }
-
-                                <TableRow>
-                                    <TableCell colSpan={3}></TableCell>
-                                    <TableCell align="right">Suma</TableCell>
-                                    <TableCell>{cartSumValue} zł</TableCell>
-                                </TableRow>
-                            </TableBody>
-                        </Table>
-                    </TableContainer>
-                </Paper>
-            </Grid>
-        </Grid >
-    );
+                <TableRow>
+                  <TableCell colSpan={3}></TableCell>
+                  <TableCell align="right">Suma</TableCell>
+                  <TableCell>{cartSumValue} zł</TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Paper>
+      </Grid>
+    </Grid>
+  );
 }
