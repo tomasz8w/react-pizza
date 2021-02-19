4f974fbd0af25921a72d00a05f42df544472b43c
tomasz8w <td.wieclaw@gmail.com>
2021-02-19 16:08:05 +0100

    Sorting list by price and name

--- src/Content.js
+++ src/Content.js
@@ -1,25 +1,62 @@
+import sortBy from 'lodash.sortby';
 import PizzaItem from './PizzaItem';
-import { useState } from "react";
-import { Checkbox, FormControl, FormHelperText, Grid, InputLabel, makeStyles, MenuItem, Select } from '@material-ui/core'
+import { useEffect, useState } from "react";
+import { IconButton, Checkbox, FormControl, FormHelperText, Grid, InputLabel, makeStyles, MenuItem, Select } from '@material-ui/core'
+import SortIcon from '@material-ui/icons/Sort';
 
 const useStyles = makeStyles((theme) => ({
   formControl: {
-    margin: theme.spacing(2)
+    margin: theme.spacing(2),
   },
-
 }));
 
 function Content(props) {
-  const data = props.data;
+  const pizzaList = props.data;
   const classes = useStyles();
   const dispatch = props.dispatch;
 
   const [selectedSize, setSelectedSize] = useState("1");
   const [onlyVegan, setOnlyVegan] = useState(false);
+  const [sort, setSort] = useState("name");
+  const [reverseSorting, setReverseSorting] = useState(false);
+  const sortedPizzaList = reverseSorting ? sortBy(pizzaList, sort) : sortBy(pizzaList, sort).reverse();
+
+  function handleSortChange(sortType) {
+    if (sortType === "name") {
+      setSort("name");
+    }
+    else if (sortType === "price") {
+      setSort(`price[${selectedSize}]`)
+    } else {
+      setSort("name");
+    }
+  }
+
+  useEffect(() => {
+    handleSortChange(sortValue);
+  }, [selectedSize, onlyVegan]);
+
+  const sortValue = (sort === "name" ? "name" : "price");
 
   return (
     <Grid container direction="row" spacing={2}>
       <Grid item xs={12} >
+        <FormControl className={classes.formControl} >
+          <InputLabel>Sortuj według</InputLabel>
+          <Select
+            onChange={(event) => {
+              handleSortChange(event.target.value);
+            }}
+            value={sortValue} >
+            <MenuItem value="name" >Nazwa</MenuItem>
+            <MenuItem value="price">Cena</MenuItem>
+          </Select>
+        </FormControl>
+        <FormControl className={classes.formControl} >
+          <IconButton onClick={() => setReverseSorting(!reverseSorting)}>
+            <SortIcon />
+          </IconButton>
+        </FormControl>
         <FormControl className={classes.formControl} >
           <InputLabel>Rozmiar dania</InputLabel>
           <Select
@@ -27,9 +64,9 @@ function Content(props) {
               setSelectedSize(event.target.value);
             }}
             value={selectedSize} >
-            <MenuItem value="0" >Maly</MenuItem>
-            <MenuItem value="1">Sredni</MenuItem>
-            <MenuItem value="2">Duzy</MenuItem>
+            <MenuItem value="0" >Mały</MenuItem>
+            <MenuItem value="1">Średni</MenuItem>
+            <MenuItem value="2">Duży</MenuItem>
           </Select>
         </FormControl>
         <FormControl className={classes.formControl}>
@@ -39,7 +76,8 @@ function Content(props) {
       </Grid>
 
       {
-        (onlyVegan ? data.filter(pizza => pizza.vegetarian) : data)
+        (onlyVegan ? sortedPizzaList.filter(pizza => pizza.vegetarian && pizza.price[selectedSize] > 0)
+          : sortedPizzaList.filter(pizza => pizza.price[selectedSize] > 0))
           .map(pizza =>
             <PizzaItem
               pizza={pizza}
