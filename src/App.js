f082c644f516d935142004d73db8779d15aabca7
tomasz8w <td.wieclaw@gmail.com>
2021-02-20 00:36:03 +0100

    cart with backdrop + dialog

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import Footer from "./Footer";
 import Content from "./Content";
 import Navigation from "./Navigation";
 import Cart from "./Cart";
-import Error from "./Error"
-import { useEffect, useReducer } from "react";
-import { Grid } from "@material-ui/core";
+import Error from "./Error";
+import { useState, useEffect, useReducer } from "react";
+import { Grid, Backdrop, makeStyles, Dialog } from "@material-ui/core";
 import { Switch, Route } from "react-router-dom";
-import useFetch from './services/useFetch'
-import cartReducer from './CartReducer'
+import useFetch from "./services/useFetch";
+import cartReducer from "./CartReducer";
 
 let initCart;
 try {
@@ -17,44 +17,71 @@ try {
   initCart = [];
 }
 
-function App() {
-  const [cart, dispatch] = useReducer(cartReducer, initCart);
+const useStyles = makeStyles((theme) => ({
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1,
+  },
+  cartDialog: {
+    flexDirection: "row",
+    fullWidth: "true",
+  },
+}));
 
-  const { data, error, loading } = useFetch("data.json");
+function App() {
+  const classes = useStyles();
+  const [cart, dispatch] = useReducer(cartReducer, initCart);
+  const [cartVisible, setCartVisible] = useState(false);
+
+  const { data, error, loading } = useFetch(
+    "https://www.maniasmaku.pl/api/v1/sites/restaurant_menu/25265/pl"
+  );
+  const pizzas = data.pizzas;
+  console.log(pizzas);
 
   useEffect(() => localStorage.setItem("cart", JSON.stringify(cart)), [cart]);
+
+  const openBackdropCart = () => {
+    setCartVisible(true);
+  };
+
+  const closeBackdropCart = () => {
+    setCartVisible(false);
+  };
 
   return (
     <Grid container direction="column">
       <Grid item xs={12}>
-        <Header />
+        <Header cart={cart} openBackdropCart={openBackdropCart} />
       </Grid>
 
+      <Backdrop className={classes.backdrop} open={cartVisible}>
+        <Dialog
+          open={cartVisible}
+          onClose={closeBackdropCart}
+          className={classes.cartDialog}
+          transitionDuration={500}
+        >
+          <Cart cart={cart} data={pizzas} dispatch={dispatch} />
+        </Dialog>
+      </Backdrop>
       <Grid item container xs={12}>
         <Grid item xs={3} sm={2}>
           <Navigation />
         </Grid>
         <Grid item xs={9} sm={9}>
-          {!loading && !error &&
+          {!loading && !error && (
             <Switch>
               <Route exact path="/">
-                <Content dispatch={dispatch} data={data} />
+                <Content dispatch={dispatch} data={pizzas} />
               </Route>
               <Route path="/cart">
-                <Cart
-                  cart={cart}
-                  data={data}
-                  dispatch={dispatch}
-                />
+                <Cart cart={cart} data={pizzas} dispatch={dispatch} />
               </Route>
             </Switch>
-          }
-          {error &&
-            <Error />
-          }
+          )}
+          {error && <Error />}
         </Grid>
       </Grid>
-
       <Grid container item xs={12} justify="center">
         <Footer />
       </Grid>
--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,40 @@
-import { AppBar, Toolbar } from '@material-ui/core';
-function Header() {
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Grid,
+  Badge,
+} from "@material-ui/core";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+
+function Header(props) {
+  const { openBackdropCart, cart } = props;
+  const numOfItemsInCart = cart
+    .map((e) => e.quantity)
+    .reduce((sum, e) => sum + e, 0);
+
   return (
     <AppBar position="static">
       <Toolbar>
-        Pizza portal
+        <Grid container alignItems="center">
+          <Grid item xs={11}>
+            <Typography>Pizza portal</Typography>
+          </Grid>
+          <Grid item xs={1}>
+            <IconButton onClick={openBackdropCart}>
+              <Badge
+                badgeContent={numOfItemsInCart}
+                style={{ color: "#ffffff" }}
+              >
+                <ShoppingCartIcon color="secondary" fontSize="large" />
+              </Badge>
+            </IconButton>
+          </Grid>
+        </Grid>
       </Toolbar>
-    </AppBar>);
-};
+    </AppBar>
+  );
+}
 
 export default Header;
