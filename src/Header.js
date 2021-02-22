dbfe183164f5943915df312ed2c6720c6053ac90
tomasz8w <td.wieclaw@gmail.com>
2021-02-22 11:56:38 +0100

    display card summary on header

--- src/Header.js
+++ src/Header.js
@@ -9,19 +9,28 @@ import {
 import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 
 function Header(props) {
-  const { openBackdropCart, cart } = props;
+  const { openBackdropCart, cart, data } = props;
+
   const numOfItemsInCart = cart
     .map((e) => e.quantity)
     .reduce((sum, e) => sum + e, 0);
 
+  const cartSumValue = cart
+    .map((cartElement) =>
+      data
+        .filter((d) => d.id === cartElement.id)
+        .map((d) => d.price[cartElement.size] * cartElement.quantity)
+    )
+    .reduce((sum, e) => sum + e[0], 0);
+
   return (
     <AppBar position="static">
       <Toolbar>
-        <Grid container alignItems="center">
+        <Grid container alignItems="center" >
           <Grid item xs={11}>
             <Typography>Pizza portal</Typography>
           </Grid>
-          <Grid item xs={1}>
+          <Grid item xs={1} container alignItems="center" justify="center">
             <IconButton onClick={openBackdropCart}>
               <Badge
                 badgeContent={numOfItemsInCart}
@@ -30,6 +39,7 @@ function Header(props) {
                 <ShoppingCartIcon color="secondary" fontSize="large" />
               </Badge>
             </IconButton>
+            <Typography>{cartSumValue} zł</Typography>
           </Grid>
         </Grid>
       </Toolbar>
