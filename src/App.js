20f21d3251e66b36aad6b3a20d184d38f6ac57cd
tomasz8w <td.wieclaw@gmail.com>
2021-02-22 14:15:18 +0100

    add few more sources

--- src/App.js
+++ src/App.js
@@ -17,6 +17,17 @@ const restaurants = [{
 {
   "name": "Roma",
   "url": "https://www.pizzeriaroma.pl/api/v1/sites/restaurant_menu/972/pl"
+}, {
+  "name": "Favola",
+  "url": "https://www.pizzerialafavola.pl/api/v1/sites/restaurant_menu/5770/pl"
+},
+{
+  "name": "Manufaktura Pizzy",
+  "url": "https://www.manufakturapizzy.pl/api/v1/sites/restaurant_menu/28740/pl"
+},
+{
+  "name": "New York Pizza",
+  "url": "https://new-york-pizza-department-legnica.skubacz.pl/api/v1/sites/restaurant_menu/29655/pl"
 }
 ]
 
